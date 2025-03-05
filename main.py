e9d4ce1b026ef2cd85af9ad637825d70e8bf1f97
koczoa <onokoczo@protonmail.com>
2025-03-05 21:31:39 +0100

    skeletons extraction from videos and images

--- main.py
+++ main.py
@@ -23,51 +23,49 @@ def skeletonMaker(array: np.ndarray, footage: np.ndarray):
     thickness = 3
     # LEFT SIDE
     color = (0, 0, 255)
-    image = cv2.line(img=footage, pt1=array[0].astype(int), pt2=array[2].astype(int), color=color,
-                     thickness=thickness)
-    image = cv2.line(img=footage, pt1=array[2].astype(int), pt2=array[4].astype(int), color=color,
-                     thickness=thickness)
-    image = cv2.line(img=footage, pt1=array[0].astype(int), pt2=array[6].astype(int), color=color,
-                     thickness=thickness)
-    image = cv2.line(img=footage, pt1=array[6].astype(int), pt2=array[8].astype(int), color=color,
-                     thickness=thickness)
-    image = cv2.line(img=footage, pt1=array[8].astype(int), pt2=array[10].astype(int), color=color,
-                     thickness=thickness)
+    cv2.line(footage, array[0].astype(int), array[2].astype(int), color, thickness)
+    cv2.line(footage, array[2].astype(int), array[4].astype(int), color, thickness)
+    cv2.line(footage, array[0].astype(int), array[6].astype(int), color, thickness)
+    cv2.line(footage, array[6].astype(int), array[8].astype(int), color, thickness)
+    cv2.line(footage, array[8].astype(int), array[10].astype(int), color, thickness)
     # RIGHT SIDE
     color = (255, 0, 0)
-    image = cv2.line(img=footage, pt1=array[1].astype(int), pt2=array[3].astype(int), color=color,
-                     thickness=thickness)
-    image = cv2.line(img=footage, pt1=array[3].astype(int), pt2=array[5].astype(int), color=color,
-                     thickness=thickness)
-    image = cv2.line(img=footage, pt1=array[1].astype(int), pt2=array[7].astype(int), color=color,
-                     thickness=thickness)
-    image = cv2.line(img=footage, pt1=array[7].astype(int), pt2=array[9].astype(int), color=color,
-                     thickness=thickness)
-    image = cv2.line(img=footage, pt1=array[9].astype(int), pt2=array[11].astype(int), color=color,
-                     thickness=thickness)
+    cv2.line(footage, array[1].astype(int), array[3].astype(int), color, thickness)
+    cv2.line(footage, array[3].astype(int), array[5].astype(int), color, thickness)
+    cv2.line(footage, array[1].astype(int), array[7].astype(int), color, thickness)
+    cv2.line(footage, array[7].astype(int), array[9].astype(int), color, thickness)
+    cv2.line(footage, array[9].astype(int), array[11].astype(int), color, thickness)
     # MIDDLE
     color = (0, 255, 0)
-    image = cv2.line(img=footage, pt1=array[0].astype(int), pt2=array[1].astype(int), color=color,
-                     thickness=thickness)
-    image = cv2.line(img=footage, pt1=array[6].astype(int), pt2=array[7].astype(int), color=color, thickness=thickness)
-    return image
+    cv2.line(footage, array[0].astype(int), array[1].astype(int), color, thickness)
+    cv2.line(footage, array[6].astype(int), array[7].astype(int), color, thickness)
 
-
-
-def main() -> int:
-    model = YOLO("yolo11x-pose.pt")
-    testFile = "resources/pelleTest.jpg"
-    result = model.track(source=testFile, save=False, show=True, name="result")
-    footage = cv2.imread(filename=testFile)
-
+def keypointDetection(result, footage):
     for r in result:
         nof = r.keypoints.shape[0]
         for p in range(nof):
             current_person_keypoints = np.delete(r.keypoints.xy[p].numpy(), slice(5), axis=0)
-            skeletonImage = skeletonMaker(current_person_keypoints, footage)
+            skeletonMaker(current_person_keypoints, footage)
 
-    cv2.imshow("skeletonised", footage)
-    cv2.waitKey(0)
+def main() -> int:
+    if len(sys.argv) < 2:
+        print(f"No filename provided!")
+        return 1
+
+    model = YOLO("yolo11n-pose.pt")
+    file = "resources/" + sys.argv[1]
+
+
+    footage = cv2.VideoCapture(filename=file)
+    while footage.isOpened():
+        success, frame = footage.read()
+        result = model.track(source=frame, save=False, show=False, name="result")
+        keypointDetection(result, frame)
+        cv2.imshow("skeletonised", frame)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    footage.release()
     cv2.destroyAllWindows()
     return 0
 
