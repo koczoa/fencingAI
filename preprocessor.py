a7636300158eebc4662e12da07a80ac46c42a24e
koczoa <onokoczo@protonmail.com>
2025-03-10 19:03:29 +0100

    preprocession and video display are separated

--- preprocessor.py
+++ preprocessor.py
@@ -1,100 +1,46 @@
+import numpy as np
 from ultralytics import YOLO
 import sys
 import cv2
-import numpy as np
-import time
 
 
-"""
-00    Left Shoulder
-01    Right Shoulder
-02    Left Elbow
-03    Right Elbow
-04    Left Wrist
-05    Right Wrist
-06    Left Hip
-07    Right Hip
-08    Left Knee
-09    Right Knee
-10    Left Ankle
-11    Right Ankle
-"""
-
-history = []
-
-
-def positionRecognition(array: np.ndarray):
-    pass
-
-def skeletonDrawer(array: np.ndarray, footage: np.ndarray) -> None:
-
-    def drawSegment(x: int, y:int, color: (int, int, int)) -> None:
-        try:
-            if not array[x].any() or not array[y].any():
-                return
-            thickness = 3
-            cv2.line(footage, array[x].astype(int), array[y].astype(int), color, thickness)
-        except IndexError:
-            pass
-
-    # LEFT SIDE
-    red = (0, 0, 255)
-    drawSegment(0, 2, red)
-    drawSegment(2, 4, red)
-    drawSegment(0, 6, red)
-    drawSegment(6, 8, red)
-    drawSegment(8, 10, red)
-
-    # RIGHT SIDE
-    blue = (255, 0, 0)
-    drawSegment(1, 3, blue)
-    drawSegment(3, 5, blue)
-    drawSegment(1, 7, blue)
-    drawSegment(7, 9, blue)
-    drawSegment(9, 11, blue)
-
-    # MIDDLE
-    green = (0, 255, 0)
-    drawSegment(0, 1, green)
-    drawSegment(6, 7, green)
-
-def keypointDetection(result, footage) -> None:
+def keypointDetection(result, history) -> None:
     for r in result:
-        keypointsRelevant = r.keypoints.xy
-        history.append(keypointsRelevant.tolist())
-        if keypointsRelevant[0].numpy().size == 0:
-            return
-        nof = keypointsRelevant.shape[0]
-        print(f"number of people detected: {nof}")
-        for p in range(nof):
-            current_person_keypoints = np.delete(keypointsRelevant[p].numpy(), slice(5), axis=0)
-            skeletonDrawer(current_person_keypoints, footage)
+        keypointsRelevant = r.keypoints.xy.numpy()
+        # print(keypointsRelevant)
+        if keypointsRelevant[0].size == 0:
+            filler = np.zeros((1, 17, 2))
+            history.append(np.array(filler))
+        else:
+            history.append(keypointsRelevant)
 
 def main() -> int:
     if len(sys.argv) < 3:
         print(f"No filename or yolo size provided!")
         return 1
+
     size = sys.argv[1]
     model = YOLO(f"yolo11{size}-pose.pt")
-    file = "resources/" + sys.argv[2]
+    fileName = sys.argv[2]
+    filePath = "resources/" + fileName + ".mp4"
+    frameCount = 0
+    history = []
 
-    footage = cv2.VideoCapture(filename=file)
+    footage = cv2.VideoCapture(filename=filePath)
     while footage.isOpened():
         success, frame = footage.read()
         if success:
             result = model.track(source=frame, save=False, show=False, name="result", persist=True, tracker="botsort.yaml")
-            keypointDetection(result, frame)
-            cv2.imshow("skeletonised", frame)
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                break
+            keypointDetection(result, history)
+            frameCount += 1
         else:
             break
-         # if cv2.waitKey(10) & 0xFF == ord('q'):
-         #    break
-    with open("saveFile", 'w') as f:
+    with open(f"processed/{fileName}_saveDump", "w") as f:
         for line in history:
-            f.write("%s\n" % line)
+            f.write(f"{line}\n")
 
+    np.save(f"processed/{fileName}.npy", np.array(history))
+    print(frameCount)
     footage.release()
     cv2.destroyAllWindows()
     return 0
---
+++ videoDisplayer.py
@@ -0,0 +1,92 @@
+import sys
+import cv2
+import numpy as np
+
+
+"""
+00    Left Shoulder
+01    Right Shoulder
+02    Left Elbow
+03    Right Elbow
+04    Left Wrist
+05    Right Wrist
+06    Left Hip
+07    Right Hip
+08    Left Knee
+09    Right Knee
+10    Left Ankle
+11    Right Ankle
+"""
+
+
+def positionRecognition(array: np.ndarray):
+    pass
+
+def skeletonDrawer(array: np.ndarray, footage: np.ndarray) -> None:
+
+    def drawSegment(x: int, y:int, color: (int, int, int)) -> None:
+        try:
+            if not array[x].any() or not array[y].any():
+                return
+            thickness = 3
+            cv2.line(footage, array[x].astype(int), array[y].astype(int), color, thickness)
+        except IndexError:
+            pass
+
+    # LEFT SIDE
+    red = (0, 0, 255)
+    drawSegment(0, 2, red)
+    drawSegment(2, 4, red)
+    drawSegment(0, 6, red)
+    drawSegment(6, 8, red)
+    drawSegment(8, 10, red)
+
+    # RIGHT SIDE
+    blue = (255, 0, 0)
+    drawSegment(1, 3, blue)
+    drawSegment(3, 5, blue)
+    drawSegment(1, 7, blue)
+    drawSegment(7, 9, blue)
+    drawSegment(9, 11, blue)
+
+    # MIDDLE
+    green = (0, 255, 0)
+    drawSegment(0, 1, green)
+    drawSegment(6, 7, green)
+
+
+def main() -> int:
+    if len(sys.argv) < 2:
+        print(f"No filename provided!")
+        return 1
+    fileName = sys.argv[1]
+    filePath = "resources/" + fileName + ".mp4"
+    frameCount = 0
+    history = np.load(f"processed/{fileName}.npy")
+
+    footage = cv2.VideoCapture(filename=filePath)
+    fps = footage.get(cv2.CAP_PROP_FPS)
+    print(fps)
+    while footage.isOpened():
+        success, frame = footage.read()
+        if success:
+            currentFrame = history[frameCount]
+            # print(f"current data: {currentFrame}")
+            for detection in currentFrame:
+                current_person_keypoints = np.delete(detection, slice(5), axis=0)
+                skeletonDrawer(current_person_keypoints, frame)
+
+            frameCount += 1
+            cv2.imshow("skeletonised", frame)
+            if cv2.waitKey(int(1000/fps)) & 0xFF == ord("q"):
+                break
+        else:
+            break
+
+    footage.release()
+    cv2.destroyAllWindows()
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
